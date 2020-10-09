4a9672c5eb1266295a38b07ee6ac9d0d86486a86
randAyo <aot14121@gmail.com>
2020-10-09 22:37:11 +0100

    3rdtest

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://imprimerie.digital';
+export const server = dev ? 'http://localhost:3000' : 'https://imprimerie-vercel.vercel.app/';
